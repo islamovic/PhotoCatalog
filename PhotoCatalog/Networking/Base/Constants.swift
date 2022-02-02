afbb5864835dca1291da61e4d89b8b3ccf9cb151
islamovic <islam.abo.elsoud@gmail.com>
2022-02-02 23:22:10 +0200

    save constants in the file

--- PhotoCatalog/Networking/Base/Constants.swift
+++ PhotoCatalog/Networking/Base/Constants.swift
@@ -8,18 +8,60 @@
 import Foundation
 
 public enum Constants {
+
     public enum Server { }
     public enum Client { }
 }
 
 extension Constants.Server {
     public static var baseUrl: String {
-        return "https://marlove.net/e/mock/v1/items"
+        let constant = Constant(fileName: "Constants")
+        return constant.baseURL
     }
 }
 
 extension Constants.Client {
     public static var token: String {
-        return "38c9495a7045f0dccebe550baa3d0d07"
+        let constant = Constant(fileName: "Constants")
+        return constant.token
+    }
+}
+
+class Constant {
+
+    private let fileName: String
+
+    init(fileName: String) {
+        self.fileName = fileName
+    }
+
+    var constants: [String: String]? {
+        guard let path = Bundle.main.path(forResource: self.fileName, ofType: "plist") else {
+            return [:]
+        }
+        let url = URL(fileURLWithPath: path)
+        guard let constantData = try? Data(contentsOf: url) else {
+            return [:]
+        }
+        guard let plistContent = try? PropertyListSerialization.propertyList(from: constantData,
+                                                                             options: .mutableContainers,
+                                                                             format: nil) as? [String: String] else {
+            return nil
+        }
+        return plistContent
+    }
+
+    var baseURL: String {
+        guard let constants = constants else {
+            return ""
+        }
+        return constants["BaseURL"] ?? ""
+    }
+
+    var token: String {
+        guard let constants = constants else {
+            return ""
+        }
+        return constants["Token"] ?? ""
     }
 }
